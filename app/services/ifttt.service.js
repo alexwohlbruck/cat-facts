0ab3b4e281aa3be3ae5f8f792d9fdb82d265f37e
alexwohlbruck <alexwohlbruck@gmail.com>
2017-01-29 19:32:13 +0000

    Any message sent via IFTTT is added to the database now

--- app/services/ifttt.service.js
+++ app/services/ifttt.service.js
@@ -1,6 +1,7 @@
 var keys = require.main.require('./app/config/keys');
 var IFTTT = require('node-ifttt-maker');
 var ifttt = new IFTTT(keys.ifttt.apiKey);
+var Message = require.main.require('./app/models/message');
 
 module.exports = {
     sendSingleMessage: function(data) {
@@ -13,6 +14,14 @@ module.exports = {
             }
         }, function(err) {
             if (err) console.log(err);
+            
+            var message = new Message({
+                text: data.message,
+                number: data.number,
+                type: 'outgoing'
+            });
+            
+            message.save();
         });
     }
 };
