f824095f821cf5dbabbe2b9d7aaf721ab0fdecc5
alexwohlbruck <alexwohlbruck@gmail.com>
2017-02-04 21:44:01 +0000

    Removed unnecessary code

--- app/models/message.js
+++ app/models/message.js
@@ -12,7 +12,3 @@ var MessageSchema = new Schema({
 var Message = mongoose.model('Message', MessageSchema);
 
 module.exports = Message;
-
-Message.remove({text: "The ancestor of all domestic cats is the African Wild Cat which still exists today."}, function(err, data) {
-    console.log(err,data);
-})
