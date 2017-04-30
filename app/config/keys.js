062d501132ad2c8008d97f9c090e6ac58713f0a1
alexwohlbruck <alexwohlbruck@gmail.com>
2017-04-30 05:03:10 +0000

    Final touches before push

--- app/config/keys.js
+++ app/config/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     oauth: {
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-        GOOGLE_REDIRECT_URL: 'https://cat-facts-alexwohlbruck.c9users.io/auth/google/contacts/callback'
+        GOOGLE_REDIRECT_URL: process.env.BASE_URL + '/auth/google/contacts/callback'
     },
     encryption: {
         key: process.env.ENCRYPTION_KEY,
