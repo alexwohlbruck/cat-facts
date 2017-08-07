cbbe70c71f48e92d3a9456030470d870b8fa1fb6
alexwohlbruck <alexwohlbruck@gmail.com>
2017-08-07 05:27:54 +0000

    Removed test unsubscribe date range

--- app/config/app-settings.js
+++ app/config/app-settings.js
@@ -8,10 +8,6 @@ const unsubscribableDates = [
     {
         start: new Date("August 8, 2017 16:20:00 EDT"),
         end: new Date("August 9, 2017 16:20:00 EDT")
-    },
-    {
-        start: new Date("August 6, 2017 01:00:00 EDT"),
-        end: new Date("August 10, 2017 01:08:30 EDT")
     }
 ],
 
