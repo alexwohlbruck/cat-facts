7ab541728358b9455754325daaf581dd066d7870
Alex Wohlbruck <alexwohlbruck@gmail.com>
2020-01-03 17:07:37 -0500

    Finish fact page (Fixes #116)

--- app/routes/catbot.routes.js
+++ app/routes/catbot.routes.js
@@ -13,224 +13,231 @@ const Message = require('../models/message');
 const Recipient = require('../models/recipient');
 const Upvote = require('../models/upvote');
 
-	
-const todayStart = new Date(); todayStart.setHours(0,0,0,0);
-const todayEnd	 = new Date(); todayEnd.setHours(23,59,59,999);
+
+const todayStart = new Date();
+todayStart.setHours(0, 0, 0, 0);
+const todayEnd = new Date();
+todayEnd.setHours(23, 59, 59, 999);
 
 
 // Get all recipients and a fact to be sent out each day
-router.get('/daily', async (req, res) => {
-	
-	if (!req.query || req.query.code !== keys.generalAccessToken) {
-		return res.status(400).json({message: "Provide the code to recieve recipients"});
-	}
-	
-	const io = req.app.get('socketio');
-	
-	const todayStart = new Date(); todayStart.setHours(0,0,0,0);
-	const todayEnd	 = new Date(); todayEnd.setHours(23,59,59,999);
-	
-	let getFactAndRecipients = async animalType => {
-		let { recipients, overrideFact, highestUpvotedFact, fact } = await Promise.props({
-			
-			recipients: Recipient.find({
-				subscriptions: animalType
-			},
-			{
-				number: 1
-			}),
-			
-			overrideFact: Fact.findOne({
-				sendDate: {
-					$gte: todayStart,
-					$lte: todayEnd
-				},
-				type: animalType
-			}),
-			
-			highestUpvotedFact: Upvote.aggregate([
-				{$group: {_id: '$fact', upvotes: {$sum: 1}}}, // Get all upvote sums
-				{$lookup: { // Retrieve associated facts
-			        from: 'facts',
-			        localField: '_id',
-			        foreignField: '_id',
-			        as: 'fact'
-			    }},
-				{$unwind: '$fact'},
-				{$match: {
-					// 'fact.used': false, // TODO: Sort by 'sentCount'
-					'fact.type': animalType,
-					'fact.sendDate': {$exists: false}
-				}}, // Only select used facts of a particular animal type
-				{$sort: {'upvotes': -1, 'fact.createdAt': 1}}, // Sort by upvote count, then by date submitted
-				{$limit: 1} // Only return one fact
-			]),
-			
-			fact: Fact.getFact({animalType})
-		});
-		
-		if (!fact) {
-			// TODO: New behavior if there are no avaialable facts
-			// ? Send error message?
-			// No unused facts are available, reset all facts to unused
-			// await Fact.update({}, {$set: {used: false}}, {multi: true});
-		}
-		
-		if (overrideFact) {
-			await overrideFact.delete();
-		}
-		
-		highestUpvotedFact = highestUpvotedFact[0] ? highestUpvotedFact[0].fact : null;
-		
-		return {
-			recipients: recipients.map(r => r.number),
-			fact: (overrideFact || highestUpvotedFact || fact).text
-		};
-	};
-	
-	const facts = {};
-	strings.animalTypes.forEach(animal => {
-		facts[animal] = getFactAndRecipients(animal);
-	});
-	
-	const result = await Promise.props(facts),
-		  dbMessages = [];
-	
-	Object.keys(result).forEach(async animal => {
+router.get('/daily', async(req, res) => {
 
-		// Increment sent count
-		await Fact.findByIdAndUpdate(result[animal].fact._id, {
-			$inc: {
-				sentCount: 1
-			}
-		});
-		
-		result[animal].recipients.forEach(async recipient => {
-			
-			// Send messages to app
-			io.emit('message', {
-				message: result[animal].fact.text, 
-				recipient: recipient
-			});
-	
-			dbMessages.push(new Message({
-				text: result[animal].fact.text,
-				number: recipient.number,
-				type: 'outgoing'
-			}));
-		});
-		
-		// Format data for response
-		result[animal] = {
-			recipients: result[animal].recipients.map(r => r.number),
-			fact: result[animal].fact.text,
-		};
-	});
-	
-	// Save messages to database
-	await Message.create(dbMessages);
-	
-	// Tweet only the cat fact
-	twitter.tweet(result.cat.fact);
+    if (!req.query || req.query.code !== keys.generalAccessToken) {
+        return res.status(400).json({ message: "Provide the code to recieve recipients" });
+    }
 
-	return res.status(200).json(result);
+    const io = req.app.get('socketio');
+
+    const todayStart = new Date();
+    todayStart.setHours(0, 0, 0, 0);
+    const todayEnd = new Date();
+    todayEnd.setHours(23, 59, 59, 999);
+
+    let getFactAndRecipients = async animalType => {
+        let { recipients, overrideFact, highestUpvotedFact, fact } = await Promise.props({
+
+            recipients: Recipient.find({
+                subscriptions: animalType
+            }, {
+                number: 1
+            }),
+
+            overrideFact: Fact.findOne({
+                sendDate: {
+                    $gte: todayStart,
+                    $lte: todayEnd
+                },
+                type: animalType
+            }),
+
+            highestUpvotedFact: Upvote.aggregate([
+                { $group: { _id: '$fact', upvotes: { $sum: 1 } } }, // Get all upvote sums
+                {
+                    $lookup: { // Retrieve associated facts
+                        from: 'facts',
+                        localField: '_id',
+                        foreignField: '_id',
+                        as: 'fact'
+                    }
+                },
+                { $unwind: '$fact' },
+                {
+                    $match: {
+                        // 'fact.used': false, // TODO: Sort by 'sentCount'
+                        'fact.type': animalType,
+                        'fact.sendDate': { $exists: false }
+                    }
+                }, // Only select used facts of a particular animal type
+                { $sort: { 'upvotes': -1, 'fact.createdAt': 1 } }, // Sort by upvote count, then by date submitted
+                { $limit: 1 } // Only return one fact
+            ]),
+
+            fact: Fact.getFact({ animalType })
+        });
+
+        if (!fact) {
+            // TODO: New behavior if there are no avaialable facts
+            // ? Send error message?
+            // No unused facts are available, reset all facts to unused
+            // await Fact.update({}, {$set: {used: false}}, {multi: true});
+        }
+
+        if (overrideFact) {
+            await overrideFact.delete();
+        }
+
+        highestUpvotedFact = highestUpvotedFact[0] ? highestUpvotedFact[0].fact : null;
+
+        return {
+            recipients: recipients.map(r => r.number),
+            fact: (overrideFact || highestUpvotedFact || fact).text
+        };
+    };
+
+    const facts = {};
+    strings.animalTypes.forEach(animal => {
+        facts[animal] = getFactAndRecipients(animal);
+    });
+
+    const result = await Promise.props(facts),
+        dbMessages = [];
+
+    Object.keys(result).forEach(async animal => {
+
+        // Increment sent count
+        await Fact.findByIdAndUpdate(result[animal].fact._id, {
+            $inc: {
+                'status.sentCount': 1
+            }
+        });
+
+        result[animal].recipients.forEach(async recipient => {
+
+            // Send messages to app
+            io.emit('message', {
+                message: result[animal].fact.text,
+                recipient: recipient
+            });
+
+            dbMessages.push(new Message({
+                text: result[animal].fact.text,
+                number: recipient.number,
+                type: 'outgoing'
+            }));
+        });
+
+        // Format data for response
+        result[animal] = {
+            recipients: result[animal].recipients.map(r => r.number),
+            fact: result[animal].fact.text,
+        };
+    });
+
+    // Save messages to database
+    await Message.create(dbMessages);
+
+    // Tweet only the cat fact
+    twitter.tweet(result.cat.fact);
+
+    return res.status(200).json(result);
 });
 
 // Text was recieved from recipient, process it and respond
 router.post('/message', (req, res) => {
-	
-	if (!req.query.query) return error({}, "No text query provided");
-	if (!req.query.number) return error({}, "No phone number provided");
-	
-	const io = req.app.get('socketio');
-	
-	var overrideMessage;
-	
-	Recipient.findOneWithDeleted({number: req.query.number})
-	
-	.then(function(recipient) {
-		
-		var promises = {};
-		
-		if (recipient) {
-			
-			if (recipient.deleted) {
-				recipient.restore();
-				overrideMessage = "Welcome back!";
-			}
-			
-			const incoming = new Message({
-				text: req.query.query,
-				number: req.query.number,
-				type: 'incoming'
-			});
-			
-			promises.recipient = recipient;
-			promises.message = incoming.save();
-			promises.catFact = Fact.getFact();
-			promises.catbotResponse = catbot.textRequest(req.query.query, {
-				sessionId: req.query.number
-			});
-		} else {
-			
-			var newRecipient = new Recipient({
-				name: req.query.name,
-				number: req.query.number.replace(/\D/g,'').replace(/^1+/, '')
-			});
-			
-			promises.recipient = newRecipient.save();
-	
-		}
-		
-		return Promise.props(promises);
-	})
-	
-	.then(result => {
-		
-		var response;
-	
-		if (result.message) {
-			if (result.catbotResponse.result && result.catbotResponse.result.fulfillment.speech) {
-				response = result.catbotResponse.result.fulfillment.speech;
-			} else {
-				response = result.catFact.text;
-			}
-		} else {
-			response = strings.welcomeMessage();
-		}
-		
-		const outgoing = new Message({text: overrideMessage || response, number: req.query.number, type: 'outgoing'});
-							
-		outgoing.save().then(message => {
-			io.emit('message', {message: message, recipient: result.recipient});
-			
-			return success(message);
-		});
-	})
-	
-	.catch(err => {
-		return error(err.message || null);
-	});
-	
-	function success(message) {
-		res.status(200).json({
-			response: message,
-			delay: computeTypingDelay(message.text),
-			number: req.query.number
-		});
-	}
-	
-	function error(error, message) {
-		error.message = message;
-		res.status(400).json(error);
-	}
-	
-	function computeTypingDelay(string) {
-		let delay = 0;
-		delay += (string.length / 2);
-		delay += Math.round(Math.random() * 10) * (Math.random() < 0.05 ? -1 : 1);
-		return Math.abs(delay) + 2;
-	}
+
+    if (!req.query.query) return error({}, "No text query provided");
+    if (!req.query.number) return error({}, "No phone number provided");
+
+    const io = req.app.get('socketio');
+
+    var overrideMessage;
+
+    Recipient.findOneWithDeleted({ number: req.query.number })
+
+    .then(function(recipient) {
+
+        var promises = {};
+
+        if (recipient) {
+
+            if (recipient.deleted) {
+                recipient.restore();
+                overrideMessage = "Welcome back!";
+            }
+
+            const incoming = new Message({
+                text: req.query.query,
+                number: req.query.number,
+                type: 'incoming'
+            });
+
+            promises.recipient = recipient;
+            promises.message = incoming.save();
+            promises.catFact = Fact.getFact();
+            promises.catbotResponse = catbot.textRequest(req.query.query, {
+                sessionId: req.query.number
+            });
+        } else {
+
+            var newRecipient = new Recipient({
+                name: req.query.name,
+                number: req.query.number.replace(/\D/g, '').replace(/^1+/, '')
+            });
+
+            promises.recipient = newRecipient.save();
+
+        }
+
+        return Promise.props(promises);
+    })
+
+    .then(result => {
+
+        var response;
+
+        if (result.message) {
+            if (result.catbotResponse.result && result.catbotResponse.result.fulfillment.speech) {
+                response = result.catbotResponse.result.fulfillment.speech;
+            } else {
+                response = result.catFact.text;
+            }
+        } else {
+            response = strings.welcomeMessage();
+        }
+
+        const outgoing = new Message({ text: overrideMessage || response, number: req.query.number, type: 'outgoing' });
+
+        outgoing.save().then(message => {
+            io.emit('message', { message: message, recipient: result.recipient });
+
+            return success(message);
+        });
+    })
+
+    .catch(err => {
+        return error(err.message || null);
+    });
+
+    function success(message) {
+        res.status(200).json({
+            response: message,
+            delay: computeTypingDelay(message.text),
+            number: req.query.number
+        });
+    }
+
+    function error(error, message) {
+        error.message = message;
+        res.status(400).json(error);
+    }
+
+    function computeTypingDelay(string) {
+        let delay = 0;
+        delay += (string.length / 2);
+        delay += Math.round(Math.random() * 10) * (Math.random() < 0.05 ? -1 : 1);
+        return Math.abs(delay) + 2;
+    }
 });
 
 module.exports = router;
--- public/js/controllers/facts.js
+++ public/js/controllers/facts.js
@@ -2,70 +2,75 @@
 var app = angular.module('catfacts');
 
 
-app.controller('FactsCtrl', ['$scope', '$rootScope', '$state', 'ApiService', 'socket',
-	function($scope, $rootScope, $state, ApiService, socket) {
-	
-	getFacts();
-	
-	$scope.submitFact = function() {
-		const factText = $scope.form.newFact;
-		
-		if (factText && factText.trim().length != 0) {
-			ApiService.submitFact({factText, animalType: $state.params.animal});
-			$scope.form.newFact = '';
-		} else {
-			$scope.showToast("Type in a fact");
-		}
-	};
-	
-	$scope.upvoteFact = function(fact) {
-		if (fact.userUpvoted) {
-			ApiService.unvoteFact(fact._id).catch(function(err) {
-				$rootScope.toast({message: err.data.message});
-			});
-		} else {
-			ApiService.upvoteFact(fact._id).catch(function(err) {
-				$rootScope.toast({message: err.data.message});
-			});
-		}
-	};
-	
-	function getFacts() {
-		$scope.promise = ApiService.getSubmittedFacts({
-			animalType: $state.params.animal
-		})
-		.then(function(response) {
-			console.log(response.data);
-			
-			$scope.facts = response.data;
-		});
-	}
-	
-	function getIndexOfFact(factID) {
-		return $scope.facts.all.map(function(o) { return o._id }).indexOf(factID);
-	}
-	
-	socket.on('fact', function(data) {
-		data.upvotes = 0;
-		$scope.facts.all.push(data);
-	});
-	
-	socket.on('fact:upvote', function(data) {
-		const factIndex = getIndexOfFact(data.fact._id);
-		$scope.facts.all[factIndex].upvotes++;
-		
-		if (data.user._id == $rootScope.authenticatedUser._id) {
-			$scope.facts.all[factIndex].userUpvoted = true;
-		}
-	});
-	
-	socket.on('fact:unvote', function(data) {
-		const factIndex = getIndexOfFact(data.fact._id);
-		$scope.facts.all[factIndex].upvotes--;
-		
-		if (data.user._id == $rootScope.authenticatedUser._id) {
-			$scope.facts.all[factIndex].userUpvoted = false;
-		}
-	});
-	
-}]);
+app.controller('FactsCtrl', ['$scope', '$rootScope', '$state', '$location', 'ApiService', 'socket',
+    function($scope, $rootScope, $state, $location, ApiService, socket) {
+
+        getFacts();
+
+        $scope.submitFact = function() {
+            const factText = $scope.form.newFact;
+
+            if (factText && factText.trim().length != 0) {
+                ApiService.submitFact({ factText, animalType: $state.params.animal });
+                $scope.form.newFact = '';
+            } else {
+                $scope.showToast("Type in a fact");
+            }
+        };
+
+        $scope.upvoteFact = function(fact) {
+            if (fact.userUpvoted) {
+                ApiService.unvoteFact(fact._id).catch(function(err) {
+                    $rootScope.toast({ message: err.data.message });
+                });
+            } else {
+                ApiService.upvoteFact(fact._id).catch(function(err) {
+                    $rootScope.toast({ message: err.data.message });
+                });
+            }
+        };
+
+        $scope.openFact = function(factId) {
+            $location.path('/' + $state.params.animal + '/facts/' + factId);
+        };
+
+        function getFacts() {
+            $scope.promise = ApiService.getSubmittedFacts({
+                    animalType: $state.params.animal
+                })
+                .then(function(response) {
+                    console.log(response.data);
+
+                    $scope.facts = response.data;
+                });
+        }
+
+        function getIndexOfFact(factID) {
+            return $scope.facts.all.map(function(o) { return o._id }).indexOf(factID);
+        }
+
+        socket.on('fact', function(data) {
+            data.upvotes = 0;
+            $scope.facts.all.push(data);
+        });
+
+        socket.on('fact:upvote', function(data) {
+            const factIndex = getIndexOfFact(data.fact._id);
+            $scope.facts.all[factIndex].upvotes++;
+
+            if (data.user._id == $rootScope.authenticatedUser._id) {
+                $scope.facts.all[factIndex].userUpvoted = true;
+            }
+        });
+
+        socket.on('fact:unvote', function(data) {
+            const factIndex = getIndexOfFact(data.fact._id);
+            $scope.facts.all[factIndex].upvotes--;
+
+            if (data.user._id == $rootScope.authenticatedUser._id) {
+                $scope.facts.all[factIndex].userUpvoted = false;
+            }
+        });
+
+    }
+]);
